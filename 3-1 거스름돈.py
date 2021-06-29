8be812ce5f4c463101323d5f26bd9db18d1bba3b
yudh1232 <86055115+yudh1232@users.noreply.github.com>
2021-06-29 22:42:36 +0900

    Update 3-1 거스름돈.py

--- 3-1 거스름돈.py
+++ 3-1 거스름돈.py
@@ -1,1 +1,36 @@
+n = 1260 # 거슬러 줘야 할 돈
+count = 0 # 동전의 수
 
+# n이 0이 될 때까지 반복
+while True:
+  if n >= 500:
+    n -= 500
+    count += 1
+  elif n >= 100:
+    n -= 100
+    count += 1
+  elif n >= 50:
+    n -= 50
+    count += 1
+  elif n >= 10:
+    n -= 10
+    count += 1
+  else:
+    break
+
+print(count) # 동전의 수 출력
+
+# 해설
+"""
+n = 1260
+count = 0
+
+# 큰 단위의 화폐부터 차례대로 확인
+coin_types = [500, 100, 50, 10]
+
+for coin in coin_types:
+  count += n // coin # 해당 화폐로 거슬러 줄 수 있는 동전의 개수 세기
+  n %= coin
+
+print(count)
+"""
