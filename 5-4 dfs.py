351cbd5eaf260aa88d83dba343d66ed8952e56f8
yudh1232 <86055115+yudh1232@users.noreply.github.com>
2021-07-01 01:12:54 +0900

    Create 5-4 dfs.py

---
+++ 5-4 dfs.py
@@ -0,0 +1,54 @@
+INF = 999999999 # 무한의 비용 선언
+
+# 2차원 리스트를 이용해 인접 행렬 표현
+graph = [
+    [0, 7, 5],
+    [7, 0, INF],
+    [5, INF, 0]
+]
+
+print(graph)
+
+# 행(Row)이 3개인 2차원 리스트로 인접 리스트 표현
+graph = [[] for _ in range(3)]
+
+# 노드 0에 연결된 기본 노드 정보 저장(노드, 거리)
+graph[0].append((1, 7))
+graph[0].append((2, 5))
+
+# 노드 1에 연결된 기본 노드 정보 저장(노드, 거리)
+graph[1].append((0, 7))
+
+# 노드 2에 연결된 기본 노드 정보 저장(노드, 거리)
+graph[2].append((0, 5))
+
+print(graph)
+
+# DFS 메서드 정의
+def dfs(graph, v, visited):
+    # 현재 노드를 방문 처리
+    visited[v] = True
+    print(v, end = ' ')
+    # 현재 노드와 연결된 다른 노드를 재귀적으로 방문
+    for i in graph[v]:
+        if not visited[i]:
+            dfs(graph, i, visited)
+
+# 각 노드가 연결된 정보를 리스트 자료형으로 표현(2차원 리스트)
+graph = [
+    [],
+    [2, 3, 8],
+    [1, 7],
+    [1, 4, 5],
+    [3, 5],
+    [3, 4],
+    [7],
+    [2, 6, 8],
+    [1, 7]
+]
+
+# 각 노드가 방문된 정보를 리스트 자료형으로 표현(1차원 리스트)
+visited = [False] * 9
+
+# 정의된 DFS 함수 호출
+dfs(graph, 1, visited)
