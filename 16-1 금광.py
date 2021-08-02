5fb30bdcaa77b230a14b801fa52b2a0e5039fbb6
yudh1232 <86055115+yudh1232@users.noreply.github.com>
2021-08-02 14:50:26 +0900

    Update 16-1 금광.py

--- 16-1 금광.py
+++ 16-1 금광.py
@@ -1,54 +1,69 @@
-# 1
-# 3 4   
-# 1 3 3 2 2 1 4 1 0 6 4 7
-
 t = int(input())
 for _ in range(t):
     n, m = map(int, input().split())
     gold_mine = [([0] * m) for _ in range(n)]
     data = list(map(int, input().split()))
     
+    # 금광 2차원 리스트 생성
     for i in range(n):
         for j in range(m):
             gold_mine[i][j] = data[m * i + j]
     
-    dx = [-1, 0, 1]
-    
-    dp_value = [-1] * (m)
-    dp_idx = [-1] * (m)
-    
-    max_value = -1
-    idx = -1
+    # DP 2차원 리스트를 생성하고 0열을 채움
+    dp = [([-1] * m) for _ in range(n)]
     for i in range(n):
-        if gold_mine[i][0] > max_value:
-            max_value = gold_mine[i][0]
-            idx = i
-    dp_value[0] = max_value
-    dp_idx[0] = idx
+        dp[i][0] = gold_mine[i][0]
     
+    # 1열부터 (m - 1)열까지
     for j in range(1, m):
-        max_value = -1
-        idx = -1
-        
+        왼쪽 위, 왼쪽, 왼쪽 아래 중 최대값을 구해서 현재 위치의 값을 구함
         for i in range(n):
-            if gold_mine[i][j] > max_value:
-                max_value = gold_mine[i][j]
-                idx = i
-                
-        if abs(idx - dp_idx[j - 1]) <= 1:
-            dp_value[j] = dp_value[j - 1] + max_value
-            dp_idx[j] = idx
-        else:
-            previous_idx = dp_idx[j - 1]
-            if previous_idx == 0:
-                next_value = max(gold_mine[previous_idx][j], gold_mine[previous_idx + 1][j])
-            if previous_idx == n - 1:
-                next_value = max(gold_mine[previous_idx][j], gold_mine[previous_idx - 1][j])
+            if i == 0:
+                dp[i][j] = max(gold_mine[i][j] + dp[i][j - 1], gold_mine[i][j] + dp[i + 1][j - 1])
+            elif i == n - 1:
+                dp[i][j] = max(gold_mine[i][j] + dp[i - 1][j - 1], gold_mine[i][j] + dp[i][j - 1])
             else:
-                next_value = max(gold_mine[previous_idx][j], gold_mine[previous_idx + 1][j], gold_mine[previous_idx - 1][j])
-            if max_value > dp_value[j - 1] + next_value:
-                height_gap = previous_idx - idx
-            
-    print('height_gap:', height_gap)
-    print('value:', dp_value)
-    print('idx:', dp_idx)
+                dp[i][j] = max(gold_mine[i][j] + dp[i - 1][j - 1], gold_mine[i][j] + dp[i][j - 1], gold_mine[i][j] + dp[i + 1][j - 1])
+    
+    # DP 2차원 리스트의 최대값을 구함 
+    result = max(map(max, dp))
+    print(result) # 결과 출력
+    
+# 해설
+"""
+# 테스트 케이스(Test Case) 입력
+for tc in range(int(input())):
+    # 금광 정보 입력
+    n, m = map(int, input().split())
+    array = list(map(int, input().split()))
+
+    # 다이나믹 프로그래밍을 위한 2차원 DP 테이블 초기화
+    dp = []
+    index = 0
+    for i in range(n):
+        dp.append(array[index:index + m])
+        index += m
+
+    # 다이나믹 프로그래밍 진행
+    for j in range(1, m):
+        for i in range(n):
+            # 왼쪽 위에서 오는 경우
+            if i == 0:
+                left_up = 0
+            else:
+                left_up = dp[i - 1][j - 1]
+            # 왼쪽 아래에서 오는 경우
+            if i == n - 1:
+                left_down = 0
+            else:
+                left_down = dp[i + 1][j - 1]
+            # 왼쪽에서 오는 경우
+            left = dp[i][j - 1]
+            dp[i][j] = dp[i][j] + max(left_up, left_down, left)
+
+    result = 0
+    for i in range(n):
+        result = max(result, dp[i][m - 1])
+
+    print(result)
+"""
